7617a66424e1d513f2bbe590162f359316096069
Husnain Akram <husnainakram525@gmail.com>
2025-07-10 17:46:54 +0500

    fixing voice chat stun/turn

--- src/components/VoiceChat.tsx
+++ src/components/VoiceChat.tsx
@@ -19,10 +19,8 @@ import {
 
 const ICE_SERVERS = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
-    { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" },
-  ],
+    { urls: "stun:stun.l.google.com:19302" }
+  ]
 }
 
 type MatchState = "idle" | "searching" | "matched" | "waiting" | "chatting"
